729f57167085a67b8dff5d67cb15d270db484d48
Keng-Wit Lim <kengwit@gmail.com>
2018-08-17 08:54:21 -0700

    add code

--- NeuralNetworks/NN3.py
+++ NeuralNetworks/NN3.py
@@ -131,6 +131,16 @@ def fully_connected(input_layer, num_outputs):
 
 my_full_output = fully_connected(my_maxpool_output, 5)
 
+def check_tensor(input_layer, num_outputs):
+    #return tf.shape(input_layer) # gives [17]
+    #return tf.stack([tf.shape(input_layer), [num_outputs]]) # gives [[17]
+                                                            #        [ 5]]
+                                                            
+    #return tf.squeeze(tf.stack([tf.shape(input_layer), [num_outputs]])) # gives [17 5]
+    return tf.expand_dims(input_layer, 0) # return [input_layer] instead of input_layer
+    
+check = check_tensor(my_maxpool_output, 5)
+
 # Initialize Variables
 init = tf.global_variables_initializer()
 sess.run(init)
@@ -166,3 +176,128 @@ print('\nInput = above array of length {}'.format(my_maxpool_output.shape.as_lis
 print('Fully connected layer on all 4 rows '
       'with {} outputs:'.format(my_full_output.shape.as_list()[0]))
 print(sess.run(my_full_output, feed_dict=feed_dict))
+
+
+#print('Check = ')
+#print(sess.run(check, feed_dict=feed_dict))
+
+
+# ===================
+# 2D data
+# ===================
+ops.reset_default_graph()
+sess=tf.Session()
+
+# Parameters for the run 
+row_size = 10 
+col_size = 10 
+conv_size = 2 
+conv_stride_size = 2 
+maxpool_size = 2 
+maxpool_stride_size = 1 
+
+
+# input array is 10x10 matrix
+data_size=[row_size,col_size]
+data_2d = np.random.normal(size=data_size)
+x_input_2d = tf.placeholder(dtype=tf.float32,shape=data_size)
+
+# convolutional layer function
+# expand data to have batch size = 1, channel size = 1
+def conv_layer_2d(input_2d,conv_filter,conv_stride):
+    input_3d = tf.expand_dims(input_2d,0)
+    input_4d = tf.expand_dims(input_3d,3)
+    #return input_4d
+            
+    convolution_output=tf.nn.conv2d(input_4d,filter=conv_filter,strides=[1,2,2,1],padding="VALID")
+
+    #input_3d = tf.expand_dims(input_2d, 0) 
+    #input_4d = tf.expand_dims(input_3d, 3) 
+
+    # Note the stride difference below! 
+    #convolution_output = tf.nn.conv2d(input_4d, 
+    #                                  filter=conv_filter, 
+    #                                  strides=[1, conv_stride, conv_stride, 1],padding="VALID") 
+
+    # Get rid of unnecessary dimensions 
+    #return tf.shape(convolution_output) # [1 5 5 1]
+    #return tf.shape(tf.squeeze(convolution_output) )
+    conv_output_2d = tf.squeeze(convolution_output) # removes extra dimensions so that shape = [5 5]
+    
+    return conv_output_2d
+    
+    
+
+# Create filter for convolution.
+# note:
+# shape of input = [batch, in_height, in_width, in_channels]
+# shape of filter = [filter_height, filter_width, in_channels, out_channels]
+# Notice that in the input, the width is on the 3rd slot but in the filter, 
+# the width is on the 2nd slot
+my_filter = tf.Variable(tf.random_normal(shape=[conv_size,conv_size,1,1]))
+my_convolution_output = conv_layer_2d(x_input_2d,my_filter,conv_stride=conv_size)
+
+
+# activation function
+def activation(input_2d):
+    return tf.nn.relu(input_2d)
+
+my_activation_output = activation(my_convolution_output)
+
+# maxpool
+def max_pool(input_2d,width,height,stride):
+    input_3d = tf.expand_dims(input_2d,0)
+    input_4d = tf.expand_dims(input_3d,3)
+    
+    # perform max pool
+    pool_output = tf.nn.max_pool(input_4d, ksize=[1, height, width, 1], 
+                                 strides=[1, stride, stride, 1], 
+                                 padding='VALID') 
+    
+    # Get rid of unnecessary dimensions 
+    pool_output_2d = tf.squeeze(pool_output) 
+    return pool_output_2d 
+
+# Create Max-Pool Layer 
+my_maxpool_output = max_pool(my_activation_output,  
+                             width=maxpool_size, 
+                             height=maxpool_size, 
+                             stride=maxpool_stride_size) 
+
+
+init = tf.global_variables_initializer() 
+sess.run(init) 
+feed_dict = {x_input_2d: data_2d}
+print('\n>>>> 2D Data <<<<') 
+
+ 
+# Convolution Output 
+print('Input = {} array'.format(x_input_2d.shape.as_list())) 
+print('{} Convolution, stride size = [{}, {}], ' 
+      'results in the {} array'.format(my_filter.get_shape().as_list()[:2], 
+                      conv_stride_size, 
+                      conv_stride_size, 
+                      my_convolution_output.shape.as_list())) 
+print(sess.run(my_convolution_output, feed_dict=feed_dict)) 
+
+ 
+# Activation Output 
+print('\nInput = the above {} array'.format(my_convolution_output.shape.as_list())) 
+print('ReLU element wise returns the {} array'.format(my_activation_output.shape.as_list())) 
+print(sess.run(my_activation_output, feed_dict=feed_dict)) 
+
+ 
+# Max Pool Output 
+print('\nInput = the above {} array'.format(my_activation_output.shape.as_list())) 
+print('MaxPool, stride size = [{}, {}], ' 
+    'results in {} array'.format(maxpool_stride_size, 
+    maxpool_stride_size, 
+    my_maxpool_output.shape.as_list())) 
+print(sess.run(my_maxpool_output, feed_dict=feed_dict)) 
+
+#323  # Fully Connected Output 
+#324  print('\nInput = the above {} array'.format(my_maxpool_output.shape.as_list())) 
+#325  print('Fully connected layer on all {} rows ' 
+#326        'results in {} outputs:'.format(my_maxpool_output.shape.as_list()[0], 
+#327                                        my_full_output.shape.as_list()[0])) 
+#print(sess.run(my_full_output, feed_dict=feed_dict)) 
