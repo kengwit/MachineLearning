8162704e7a5f6e20e31e6a8f41e9e3892bae698f
Keng-Wit Lim <kengwit@gmail.com>
2019-01-10 19:48:58 -0800

    add code

--- Documents/notes/python/chi_square_test.py
+++ Documents/notes/python/chi_square_test.py
@@ -1,19 +1,22 @@
+import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats as sst
 from scipy.stats import norm
+#np.random.seed(42)
 
 n = 20
 nsim = 10000
-x = np.random.normal(0, 1, (20,nsim))
+x = np.random.normal(0, 1, (n,nsim))
 # create four bins with cutpoints at the quartiles of a standard normal: -0.675, 0, +0.657,
 # i.e., create for 4 bins of standard normal for probabilities [0., 0.25, 0.5, 0.75, 1.0]
 bins = norm.ppf( np.linspace(0,1,num=5) )  
 
 # Compute statistics.
 #
-x = np.zeros(shape=(5,2))
-for i in range(0,5):
-    x[i,0] = i
-    x[i,1] = 2*i
+#x = np.zeros(shape=(5,2))
+#for i in range(0,5):
+#    x[i,0] = i/10
+#    x[i,1] = 2*i/10
     
 m = x.mean(axis=0)
 s = np.std(x, axis=0, ddof=0) # note: ddof=0 is the max likelihood estimate, ddof=1 is the unbiased estimate
@@ -26,3 +29,73 @@ s = np.std(x, axis=0, ddof=0) # note: ddof=0 is the max likelihood estimate, ddo
 
 
 # for a given column, bin the elements in that column
+#def get_counts(x,bin_seq):
+#    ncols = np.shape(x)[1]
+#    nbins = len(bin_seq)
+#    counts = np.zeros(shape=(nbins,ncols))
+#    for i in range(0,ncols):        
+#        print(np.histogram(x[:,i], bins=bin_seq))
+#    
+#    return counts
+#
+#counts = get_counts(x,bins)
+
+
+#def my_func(a):
+#    return a.sum()
+#b = np.array([[1,2,3], [4,5,6], [7,8,9]])
+#print( np.apply_along_axis(my_func, 0, b) )
+#counts2, bin_edges = np.apply_along_axis(lambda a: np.histogram(a, bins=bins), 0, x)
+def get_counts(x,bin_seq):
+    ncols = np.shape(x)[1]
+    nbins = len(bin_seq)-1
+    counts = np.zeros(shape=(nbins,ncols))
+    for i in range(0,ncols):
+        xi = x[:,i]
+        hist, bin_edges = np.histogram(xi, bins=bin_seq)
+        counts[:,i] = hist
+    return counts
+
+counts = get_counts(x,bins)
+
+def get_expectations(x,m,s,bin_seq):
+    
+    n     = np.shape(x)[0]
+    ncols = np.shape(x)[1]
+    nbins = len(bin_seq)-1
+    expectations = np.zeros(shape=(nbins,ncols))
+    
+    for i in range(0,ncols):
+        mi = m[i]
+        si = s[i]
+        
+        # note: 
+        # The equivalent of the R pnorm() function is: scipy.stats.norm.cdf() with python 
+        # The equivalent of the R qnorm() function is: scipy.stats.norm.ppf() with python
+        cdf_i = sst.norm.cdf(bin_seq,loc=mi,scale=si)
+        
+        exp_i = n*np.diff(cdf_i)
+        # bin the cdf
+        #exp_i, bin_edges_i = np.histogram(exp_i, bins=bin_seq)
+        
+        # get the counts in each bin
+        #exp_i = n*exp_i
+        #print(exp_i.reshape(4,5))
+        #print( np.shape( exp_i ))
+        expectations[:,i] = exp_i
+    
+    return expectations
+
+expectations = get_expectations(x,m,s,bins)
+
+plt.figure()
+plt.hist(m, bins=20,density=True)  # arguments are passed to np.histogram
+x_axis = np.arange(-2, 2, 0.001)
+plt.plot(x_axis, norm.pdf(x_axis,loc=0.0,scale=1/np.sqrt(n)))
+plt.xlim(-0.75,0.75)
+
+plt.figure()
+plt.hist(m, bins=20,density=True)  # arguments are passed to np.histogram
+x_axis = np.arange(-2, 2, 0.001)
+plt.plot(x_axis, norm.pdf(x_axis,loc=0.0,scale=1/np.sqrt(n)))
+plt.xlim(-0.75,0.75)
--- Documents/notes/python/chi_square_test.r
+++ Documents/notes/python/chi_square_test.r
@@ -3,6 +3,10 @@
 #
 n <- 20
 n.sim <- 1e4
+
+#n <- 5
+#n.sim <- 3
+
 bins <- qnorm(seq(0, 1, 1/4))
 x <- matrix(rnorm(n*n.sim), nrow=n)
 #
@@ -15,17 +19,21 @@ m <- colMeans(x)
 # compute std dev of each column
 s <- apply(sweep(x, 2, m), 2, sd)
 
+# for each column, count and bin the elements (rows)
 # create "count" where
 # rows are bins 1,2,3,4
 # cols are the 10,000 simulations
 counts <- apply(matrix(as.numeric(cut(x, bins)), nrow=n), 2, tabulate, nbins=4)
 
 #   for each m[i], s[i] pair, i = 1,...,n.sim
-#      generate pdf[i] = pnorm(bins, m[i], s[i]) corresponding to the vector of percentiles given by "bins"
-#      calculate diff( pdf[i] ) i.e. calculate the increment sizes in the "Y-axis"
+#      generate cdf[i] = pnorm(bins, m[i], s[i]) corresponding to the vector of percentiles given by "bins"
+#      calculate diff( cdf[i] ) i.e. calculate the increment sizes in the "Y-axis"
+#      calculate the expected number of entities in each bin by multiplying diff( cdf[i] ) with n
 expectations <- mapply(function(m,s) n*diff(pnorm(bins, m, s)), m, s)
 
-# colSums sums each column
+# compute the chi-squared statistic for each column: for each column,
+#  1) for each element (row), calculate chi2 = ( count-expectation )^2/expectation
+#  2) sum all the chi2's; this is done using colSums function
 chisquared <- colSums((counts - expectations)^2 / expectations)
 #
 # Plot histograms of means, variances, and chi-squared stats.  The first
@@ -35,13 +43,21 @@ mfrow <- par("mfrow")
 par(mfrow=c(1,3))
 red <- "#a04040"  # Intended to show correct distributions
 blue <- "#404090" # To show the putative chi-squared distribution
+
+# plot 1
 hist(m, freq=FALSE)
 curve(dnorm(x, sd=1/sqrt(n)), add=TRUE, col=red, lwd=2)
+
+# plot 2
 hist(s^2, freq=FALSE)
 curve(dchisq(x*(n-1), df=n-1)*(n-1), add=TRUE, col=red, lwd=2)
+
+
+# plot 3
 hist(chisquared, freq=FALSE, breaks=seq(0, ceiling(max(chisquared)), 1/4), 
      xlim=c(0, 13), ylim=c(0, 0.55), 
      col="#c0c0ff", border="#404040")
 curve(ifelse(x <= 0, Inf, dchisq(x, df=2)), add=TRUE, col=red, lwd=2)
 curve(ifelse(x <= 0, Inf, dchisq(x, df=1)), add=TRUE, col=blue, lwd=2)
+
 par(mfrow=mfrow)
